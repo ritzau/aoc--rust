70c46df354ddd58c4e7128a103aba1a8ce36977e
Tobias Ritzau <tobias@ritzau.se>
2024-12-11 12:36:12 +0100

    Solve s24e11

--- crates/aoc/src/s24.rs
+++ crates/aoc/src/s24.rs
@@ -12,6 +12,7 @@ mod e07;
 mod e08;
 mod e09;
 mod e10;
+mod e11;
 
 const YEAR: Year = Year(2024);
 
@@ -29,6 +30,7 @@ pub fn solve() -> PuzzleResult<()> {
         e08::solve,
         e09::solve,
         e10::solve,
+        e11::solve,
     ])
 }
 
